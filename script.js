2c044dce8132854f0d261e838b65f62cd834206f
benyaar <ghostmatt36@gmail.com>
2021-12-08 18:01:07 +0200

    first commit

--- script.js
+++ script.js
@@ -1,58 +1,63 @@
-fetch('https://random-data-api.com/api/users/random_user?size=10')
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data)
-    let cardName =  document.getElementById("person")
-    let res_data = data;
-    for (let i = 0; i < res_data.length; i++) {
-        let optVal = document.createElement('option')
-        optVal.innerHTML = res_data[i].first_name
-        cardName.appendChild(optVal)     
+let n1 = "";
+let n2 = "";
+let sign = "";
+// let finish = false;
+
+const out = document.querySelector(".calculator_screen");
+const clear = document.querySelector(".clear");
+const value = document.querySelector(".btn");
+const action = document.querySelector(".act");
+
+document.querySelector(".buttons").addEventListener("click", (e) => {
+  if (!e.target.classList.contains("btn")) return;
+  if (e.target.classList.contains("clear")) {
+    n1 = "";
+    n2 = "";
+    sign = "";
+    finish = false;
+    out.textContent = 0;
+    console.log(0);
+    return;
+  }
+  if (e.target.classList.contains("number")) {
+    if (n2 === "" && sign === "") {
+      n1 += e.target.getAttribute("data-value");
+      out.innerHTML = n1;
+    } else if (n1 !== "" && n2 !== "" && finish) {
+      n2 = e.target.getAttribute("data-value");
+      finish = false;
+      out.innerHTML = n2;
+    } else {
+      n2 += e.target.getAttribute("data-value");
+      out.innerHTML = n2;
     }
-    let card = document.getElementById('card')
-    let changeName = document.getElementById("person")
+  }
 
-changeName.addEventListener('change', () =>{
-    console.log(changeName.value);
-    card.innerHTML = ''
-    for (let i = 0; i < res_data.length; i++) {
-        function createCardKey(user) {
-            if (changeName.value == res_data[i].first_name) {
-                let newName = document.createElement('p')
-                newName.innerHTML = user;
-                card.appendChild(newName) 
-                let img = document.getElementById('imgsize')
-                img.src = res_data[i].avatar
-            }   
-        }
-        createCardKey(`First_name: ${res_data[i].first_name}`)
-        createCardKey(`Last_name: ${res_data[i].last_name}`)
-        createCardKey(`Date of birth: ${res_data[i].date_of_birth}`)
-        createCardKey(`Phone number: ${res_data[i].phone_number}`)
-        createCardKey(`Username: ${res_data[i].username}`)
-        createCardKey(`Email: ${res_data[i].email}`)
-        createCardKey(`Gender: ${res_data[i].gender}`)
+  if (e.target.classList.contains("act")) {
+    sign = e.target.getAttribute("data-value");
+    out.innerHTML = sign;
+    return;
+  }
+  if (e.target.classList.contains("equal")) {
+    if (n2 === '') n2=n1 
+    switch (sign) {
+      case "+":
+        n1 = +n1 + +n2;
+        break;
+      case "-":
+        n1 = n1 - n2;
+        break;
+      case "*":
+        n1 = n1 * n2;
+        break;
+      case "/":
+        n1 = n1 / n2;
+        break;
     }
-})    
-})
-
-// /////////////////////////////////////////////////////////////////////////////
-
-let color = document.querySelector('.color');
-color.addEventListener('click', () => {
-    fetch( 'https://random-data-api.com/api/color/random_color')
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        let colorData = data
-        color.style.backgroundColor = colorData.hex_value;
-    })
-})
-
-
-
-      
-
+    finish = true;
+    out.innerHTML = n1;
+  }
+  if (out.length < 11) {
+    
+  }
+});
